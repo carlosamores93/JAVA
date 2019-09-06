639838d48f1a96328798983bd1e7a66617c00132
Carlos S Amores M <carlos.amores@iahorro.com>
2019-09-06 15:01:40 +0200

    Creando CPUs, obejtos y la clase

--- src/main/java/ListaOficinas/Main.java
+++ src/main/java/ListaOficinas/Main.java
@@ -16,10 +16,20 @@ public class Main {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        // TODO code application logic here
-        System.out.println("Carlos Amores");
         
-        // Creamos un objeto monitor
+        
+        Cpu intel3 = new Cpu("i3", 2, 4);
+        Cpu intel5 = new Cpu("i5", 2, 8);
+        Cpu intel7 = new Cpu("i7", 3, 12);
+        Cpu amd = new Cpu("AMD", 2, 8);
+        System.out.println(intel3.toString());
+        System.out.println(intel5.toString());
+        System.out.println(intel7.toString());
+        System.out.println(amd.toString());
+
+        
+        
+        
         Monitor monitor1 = new Monitor("Dell", "HDMI", 24);
         Monitor monitor2 = new Monitor("HP", "VGA", 24);
         Monitor monitor3 = new Monitor("ASUS", "HDMI", 20);        
@@ -32,6 +42,10 @@ public class Main {
         
         Ram ram1 = new Ram("MARCA", 8);
         System.out.println(ram1.toString()); 
+        Ram ram2 = new Ram("DELL", 4);
+        System.out.println(ram2.toString()); 
+        Ram ram3 = new Ram("TOSS", 12);
+        System.out.println(ram3.toString()); 
         
         Rom rom1 = new Rom("DELL", 256);
         Rom rom2 = new Rom("Toshiba", 512);
