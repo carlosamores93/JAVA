b90ea4b6b3e1547ac7fcf3b5efc9385bd75c858e
Carlos <amorescarlos93@hotmail.com>
2019-09-08 22:07:27 +0200

    Agrenado CPU celeron desde mi PC :)

---
+++ ListaOficinas/src/main/java/ListaOficinas/Main.java
@@ -0,0 +1,63 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package ListaOficinas;
+import com.oficinas.*;
+
+/**
+ *
+ * @author carlos
+ */
+public class Main {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        
+        
+        Cpu intel3 = new Cpu("i3", 2, 4);
+        Cpu intel5 = new Cpu("i5", 2, 8);
+        Cpu intel7 = new Cpu("i7", 3, 12);
+        Cpu amd = new Cpu("AMD", 2, 8);        
+        Cpu celeron = new Cpu("Celeron", 1, 2);
+
+        System.out.println(intel3.toString());
+        System.out.println(intel5.toString());
+        System.out.println(intel7.toString());
+        System.out.println(amd.toString());
+        System.out.println(celeron.toString());
+
+        
+        
+        
+        Monitor monitor1 = new Monitor("Dell", "HDMI", 24);
+        Monitor monitor2 = new Monitor("HP", "VGA", 24);
+        Monitor monitor3 = new Monitor("ASUS", "HDMI", 20);        
+        Monitor monitor4 = new Monitor("ACER", "HDMI/VGA", 30);
+        System.out.println(monitor1.toString());
+        System.out.println(monitor2.toString());
+        System.out.println(monitor3.toString());
+        System.out.println(monitor4.toString());
+        
+        
+        Ram ram1 = new Ram("MARCA", 8);
+        System.out.println(ram1.toString()); 
+        Ram ram2 = new Ram("DELL", 4);
+        System.out.println(ram2.toString()); 
+        Ram ram3 = new Ram("TOSS", 12);
+        System.out.println(ram3.toString()); 
+        
+        Rom rom1 = new Rom("DELL", 256);
+        Rom rom2 = new Rom("Toshiba", 512);
+        Rom rom3 = new Rom("Kingstioon SSD", 128);
+        System.out.println(rom1.toString());
+        System.out.println(rom2.toString());
+        System.out.println(rom3.toString());
+
+        
+    }
+    
+}
