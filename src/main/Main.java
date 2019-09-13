88f72a1ff8edb184cb0d7e99db77caee63628744
Carlos <amorescarlos93@hotmail.com>
2019-09-13 19:41:05 +0200

    Cosillas

--- src/main/Main.java
+++ src/main/Main.java
@@ -18,6 +18,7 @@ public class Main {
     public static void main(String[] args) {
         
         repasoConceptosGeneralesDeLaPoo();
+        //probandoCosillas();
      
     }
     
@@ -26,4 +27,69 @@ public class Main {
         System.out.println(ofi.toString());
     }
     
+    
+    public static void probandoCosillas(){
+        /*
+        * Test Comentario
+        * XD
+        * */
+       System.out.println("Carlos Amores Martinez");
+
+       /*
+        * Variables: dir de memoria para almacenar un valor
+        * Tipos de variables o datos:
+        * 	byte, short, int, long, char, float, double y boolean
+        * 
+        *		Nombre	bytes	Rango
+        *		long	8		–9,223,372,036,854,775,808 a 9,223,372,036,854,775,807
+        *		int		4		–2,147,483,648 a 2,147,483,647
+        *		short	2		–32,768 to 32,767
+        *		byte	1		–128 to 127
+        *
+        *		double	8		4.9e–324 to 1.8e+308
+        *		float	4		1.4e–045 to 3.4e+038
+        * */
+
+       int version = 9;
+       System.out.println("Valor de la variable 'version'=" + version);
+
+       float variable_float = 9.8f;
+       char letra = 'a';
+       char numero_char = 97;
+       System.out.println("letra=" + letra + " -- numero_char=" + numero_char);
+
+       boolean ok = true;
+       boolean ko = false;
+
+       byte diasMes = 31; //Aproximadamente
+       short diasLustro = (12 * 31) * 5;
+       int velocidadLuz = 299792458;
+       long añoLuz = velocidadLuz * 365;
+
+       float pi = 3.1415926535f;
+       double e = 2.718281828459045235360;
+
+       /*
+        * 		\n. Salto de línea.
+        *		\t. Tabulador.
+        *		\\. Diagonal inversa.
+        *		\". Comillas dobles.
+        *		\'. Comillas simples.
+        *
+        */
+       System.out.print("Hola,\nagregamos un salto");
+       System.out.print("\tagregamos un tab");
+       System.out.println("");
+
+       System.out.println("Mensaje con \\ diagonal inversa");
+       System.out.println("Mensaje con \" comillas dobles");
+       System.out.println("Mensaje con \' comillas simples");
+
+
+
+       /*
+        * Constante
+        * */
+       final int puerto_bbdd = 21;
+    }
 }
